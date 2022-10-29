a6817e6c213a4527237504f449afa41d737ec71d
Kremerin <110621622+kremerin@users.noreply.github.com>
2022-10-29 13:53:16 +0200

    Morse Code

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+  ];
+let message = document.getElementById("message")
+let output1 = document.getElementById("output1");
+let output2 = document.getElementById("output2")
+let output3 = document.getElementById("output3");
+
+
+let morse = () => {
+  for (input of message.value.toUpperCase()) {
+    for (match of morseAlphabet) {
+      if (match.letter === input) {
+        output1.innerHTML += match.morseCode;
+        output3.innerHTML += match.morseCode + '<br>';
+        if (match.letter === ' ') {
+          output2.innerHTML += '<br>';
+          } else {
+            output2.innerHTML += match.letter + ' = ' + match.morseCode + '<br>';
+          }
+      }
+    }
+  }
+};
+  
